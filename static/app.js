9ba9b8c2ae4e8b915b7b43d8e90cb3ddd5057eff
pradipraodeo <raodeo@gmail.com>
2022-10-02 21:14:27 -0400

    Initial version code

--- static/app.js
+++ static/app.js
@@ -24,3 +24,33 @@ function optionChanged(sampleID) {
     IndividualInfo(sampleID);
     buildGaugePlot(sampleID);
 }
+// function for creating the bar chart with the selected ID from the dropdown
+function buildBarPlot(SelectedsampleID) {
+
+    d3.json("samples.json").then(function(data) {
+            var DataNeeded = data.samples.filter(object =>object.id.toString() == SelectedsampleID)[0];
+            console.log(DataNeeded);
+
+           // Create the Trace
+            var trace = {
+                x : DataNeeded.sample_values.slice(0,10).reverse(),
+                y : DataNeeded.otu_ids.slice(0,10).reverse().map (row => "OTU " + row),
+                text: DataNeeded.otu_labels.slice(0,10).reverse(),
+                type : "bar",
+                orientation : "h"
+            };
+
+            // Create the data array for the plot
+            var data = [trace];
+
+            // Define the plot layout
+            var layout = {
+                height: 600,
+                width: 500
+              };
+
+            // Plot the chart to a div tag with id "bar"
+            Plotly.newPlot("bar", data, layout);
+  })
+
+}
